e0b2af7baea7c8674118dc1bf063ab71c1306224
raxp <raxp.worm202@gmail.com>
2022-04-25 11:33:03 +0400

    Removed video prediction

--- opennsfw2/__init__.py
+++ opennsfw2/__init__.py
@@ -4,7 +4,6 @@ from ._image import preprocess_image as preprocess_image
 from ._inference import Aggregation
 from ._inference import predict_image as predict_image
 from ._inference import predict_images as predict_images
-from ._inference import predict_video_frames as predict_video_frames
 from ._model import make_open_nsfw_model as make_open_nsfw_model
 
 __version__ = "0.10.2"
--- opennsfw2/_inference.py
+++ opennsfw2/_inference.py
@@ -4,7 +4,6 @@ Inference utilities.
 from enum import auto, Enum
 from typing import Any, Callable, List, Optional, Sequence, Tuple
 
-import cv2  # type: ignore
 import numpy as np
 import tensorflow as tf  # type: ignore
 from PIL import Image  # type: ignore
@@ -92,93 +91,3 @@ def _get_aggregation_fn(
         return float(agg(x))
 
     return fn
-
-
-def predict_video_frames(
-        video_path: str,
-        frame_interval: int = 8,
-        aggregation_size: int = 8,
-        aggregation: Aggregation = Aggregation.MEAN,
-        batch_size: int = 8,
-        output_video_path: Optional[str] = None,
-        preprocessing: Preprocessing = Preprocessing.YAHOO,
-        weights_path: Optional[str] = get_default_weights_path(),
-        progress_bar: bool = True
-) -> Tuple[List[float], List[float]]:
-    """
-    Make prediction for each video frame.
-    """
-    cap = cv2.VideoCapture(video_path)  # pylint: disable=no-member
-    fps = cap.get(cv2.CAP_PROP_FPS)  # pylint: disable=no-member
-
-    model = make_open_nsfw_model(weights_path=weights_path)
-
-    video_writer: Optional[cv2.VideoWriter] = None  # pylint: disable=no-member
-    input_frames: List[NDFloat32Array] = []
-    nsfw_probability = 0.0
-    nsfw_probabilities: List[float] = []
-    frame_count = 0
-
-    if progress_bar:
-        pbar = tqdm(total=int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))  # pylint: disable=no-member
-    else:
-        pbar = None
-
-    while cap.isOpened():
-        ret, bgr_frame = cap.read()  # Get next video frame.
-        if not ret:
-            break  # End of given video.
-
-        if pbar is not None:
-            pbar.update(1)
-
-        frame_count += 1
-        frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2RGB)  # pylint: disable=no-member
-
-        if video_writer is None and output_video_path is not None:
-            video_writer = cv2.VideoWriter(  # pylint: disable=no-member
-                output_video_path,
-                cv2.VideoWriter_fourcc(*"mp4v"),  # pylint: disable=no-member
-                fps, (frame.shape[1], frame.shape[0])
-            )
-
-        if frame_count == 1 or (frame_count + 1) % frame_interval == 0:
-            pil_frame = Image.fromarray(frame)
-            input_frame = preprocess_image(pil_frame, preprocessing)
-            input_frames.append(input_frame)
-
-            if frame_count == 1 or len(input_frames) >= aggregation_size:
-                predictions = model.predict(
-                    tf.convert_to_tensor(input_frames),
-                    batch_size=batch_size, verbose=0
-                )
-                agg_fn = _get_aggregation_fn(aggregation)
-                nsfw_probability = agg_fn(predictions[:, 1])
-                input_frames = []
-
-        nsfw_probabilities.append(nsfw_probability)
-
-        if video_writer is not None:
-            prob_str = str(np.round(nsfw_probability, 2))
-            result_text = f"NSFW probability: {prob_str}"
-            # RGB colour.
-            colour = (255, 0, 0) if nsfw_probability >= 0.8 else (0, 0, 255)
-            cv2.putText(  # pylint: disable=no-member
-                frame, result_text, (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,  # pylint: disable=no-member
-                1, colour, 2, cv2.LINE_AA  # pylint: disable=no-member
-            )
-            video_writer.write(
-                cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # pylint: disable=no-member
-            )
-
-    if video_writer is not None:
-        video_writer.release()
-    cap.release()
-    cv2.destroyAllWindows()  # pylint: disable=no-member
-
-    if pbar is not None:
-        pbar.close()
-
-    elapsed_seconds = (np.arange(1, len(nsfw_probabilities) + 1) / fps).tolist()
-    return elapsed_seconds, nsfw_probabilities
